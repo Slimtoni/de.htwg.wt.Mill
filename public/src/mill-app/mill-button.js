323f02ab59551bf2497f1cb6dd39ae3dc25ee128
Matthias Asche <matthiaskg.asche@gmail.com>
2019-12-10 21:22:16 +0100

    polymer works

--- public/src/mill-app/mill-button.js
+++ public/src/mill-app/mill-button.js
@@ -1,13 +1,33 @@
-import {html, LitElement, property} from "https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module";
+import {css, html, LitElement} from "https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module";
+
 
 
 class MillButton extends LitElement {
-    @property() data = '';
 
+    static get styles() {
+        return [css`
+      .btn-primary {
+            background-color: #e05435;
+            border: #e05435;
+       }
+       
+      .btn-primary:hover{
+            background-color: #5CBBB7;
+       }
+    `];
+    }
+
+    static get properties() {
+        return { data: { type: String } };
+    }
+    constructor(){
+        super();
+        this.data = '';
+    }
 
     render() {
         return html`
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <a class="btn btn-primary" href="/start" role="button">${this.data}</a>
         `;
     }
