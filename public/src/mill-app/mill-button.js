49b78bbbe0deedd53439f53a2d27d2a6af9ac2d6
Matthias Asche <matthiaskg.asche@gmail.com>
2019-12-10 19:54:23 +0100

    more polymer

--- public/src/mill-app/mill-button.js
+++ public/src/mill-app/mill-button.js
@@ -1,12 +1,13 @@
-import {LitElement, html, css, customElement, property} from 'lit-element';
+import {html, LitElement, property} from "https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module";
 
-@customElements('mill-button')
-export class MillButton extends LitElement {
+
+class MillButton extends LitElement {
     @property() data = '';
 
 
-    render(){
+    render() {
         return html`
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <a class="btn btn-primary" href="/start" role="button">${this.data}</a>
         `;
     }
