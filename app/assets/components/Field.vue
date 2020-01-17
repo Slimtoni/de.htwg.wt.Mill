43a0b1e431ba7f642c671835988885126f066bf0
Anton Hummel <anton.hummel@htwg-konstanz.de>
2020-01-17 10:32:08 +0100

    Mill-Vue_optimization: implementing move and fly

--- app/assets/components/Field.vue
+++ app/assets/components/Field.vue
@@ -23,22 +23,48 @@
             clickHandler: async function () {
                 console.log("Field " + this.id + " clicked!");
                 this.$root.updateGameboard();
-
                 if (this.$root.playerOnTurn !== undefined) {
                     if (!this.$root.foundMill) {
                         if (this.$root.playerOnTurnPhase === "Place") {
                             this.$root.performTurn(this.id, undefined);
                             this.$root.checkMill(this.id);
-                            console.log("foundMill before sleep: " + this.$root.foundMill)
                             await this.$root.sleep(50);
-                            console.log("foundMill after sleep: " + this.$root.foundMill)
                             if (!this.$root.foundMill) {
                                 console.log("No Mill found!!!");
+                                await this.$root.sleep(50);
                                 this.$root.endPlayersTurn()
+                            }
+                        } else {
+                            if (this.$root.firstClick) {
+                                if (this.$root.gameboard[this.id]["status"] === this.$root.playerOnTurn.toLowerCase()) {
+                                    this.$root.clickOne = this.id;
+                                    this.$root.firstClick = false;
+                                    console.log("Field " + this.id + " selected! Choose second Field to " + this.$root.playerOnTurnPhase + "1");
+                                } else {
+                                    console.log("Please select one of your own mens to " + this.$root.playerOnTurnPhase)
+                                }
+                            } else {
+                                if (this.id === this.$root.clickOne) {
+                                    this.$root.clickOne = 0;
+                                    this.$root.firstClick = false;
+                                    console.log("Field " + this.id + " diselected! Choose a Man to " + this.$root.playerOnTurnPhase + "!");
+                                } else {
+                                    this.$root.performTurn(this.$root.clickOne, this.id);
+                                    this.$root.checkMill(this.id);
+                                    await this.$root.sleep(50);
+                                    if (!this.$root.foundMill) {
+                                        console.log("No Mill found!!!");
+                                        await this.$root.sleep(50);
+                                        this.$root.endPlayersTurn()
+                                    }
+                                }
                             }
                         }
                     } else {
-                        this.$root.caseOfMill(this.id)
+                        this.$root.caseOfMill(this.id);
+                        await this.$root.sleep(50);
+                        this.$root.foundMill = false;
+                        this.$root.endPlayersTurn();
                     }
                 }
             }
